9f335e44590412a859e9d494a577ca9ef9e91743
Nadeen <75531472+NadeenXo@users.noreply.github.com>
2025-05-08 17:57:18 +0300

    merge with dev

--- src/main/kotlin/logic/module/exceptions/WeatherExceptions.kt
+++ src/main/kotlin/logic/module/exceptions/WeatherExceptions.kt
@@ -1,5 +1,4 @@
 package org.baghdad.logic.module.exceptions
 
-class InvalidWeatherApiKey(message: String) : Exception(message)
 class ErrorFetchingWeatherData(message: String) : Exception(message)
 class NotValidCityNameException(msg: String): Exception(msg)
