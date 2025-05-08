ca69d712523abc361b6456a9f34f25dad6c6b93e
Nadeen <75531472+NadeenXo@users.noreply.github.com>
2025-05-08 18:10:20 +0300

    merged with dev

--- src/main/kotlin/di/appModule.kt
+++ src/main/kotlin/di/appModule.kt
@@ -1,12 +1,25 @@
 package di
+
+import data.source.weather.ApiWeatherDataSource
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.baghdad.data.repository.clothes.ClothesRepositoryImpl
+import org.baghdad.data.repository.weather.WeatherRepositoryImpl
+import org.baghdad.data.source.WeatherDataSource
+import org.baghdad.data.source.remote.clothes.MongoClientProvider
+import org.baghdad.data.source.remote.clothes.MongoClothesDataSource
+import org.baghdad.data.source.remote.clothes.MongoClothesMapper
+import org.baghdad.logic.repository.WeatherRepository
 import org.koin.dsl.module
 
 val appModule = module {
     single { MongoClientProvider() }
     single { MongoClothesMapper() }
-    single { MongoClothesDataSource(get(),get())}
+    single { MongoClothesDataSource(get(), get()) }
     single { ClothesRepositoryImpl(get()) }
-val appModule = module{
 
     // Ktor HTTP Client with JSON serialization
     single {
@@ -22,6 +35,7 @@ val appModule = module{
 
     // Remote data source
     single<WeatherDataSource> { ApiWeatherDataSource(get()) }
+
     // Repositories
     single<WeatherRepository> { WeatherRepositoryImpl(get()) }
 
