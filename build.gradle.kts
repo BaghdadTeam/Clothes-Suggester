766c5373b384dc281a06c8765b75d1878068ba4b
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-05-08 23:03:48 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,7 +85,7 @@ tasks.jacocoTestReport {
             exclude("**/model/**") // Exclude entities package
             exclude("**/generated/**") // Exclude generated code if any
             exclude("**/main.kt") // Exclude main.kt file
-            exclude("**/apiweatherdatasource.kt") // Exclude api connection file
+            exclude("**/ApiWeatherDataSource.class") // Exclude api connection file
 
         }
     )
@@ -104,7 +104,7 @@ tasks.jacocoTestCoverageVerification {
             exclude("**/di/**")
             exclude("**/model/**")
             exclude("**/main.kt")
-            exclude("**/apiweatherdatasource.kt")
+            exclude("**/ApiWeatherDataSource.class")
         }
     )
     sourceDirectories.setFrom(files("src/main/kotlin"))
@@ -149,7 +149,7 @@ tasks.named<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
             exclude("**/di/**")
             exclude("**/model/**")
             exclude("**/main.kt")
-            exclude("**/apiweatherdatasource.kt")
+            exclude("**/ApiWeatherDataSource.class")
         }
     )
     sourceDirectories.setFrom(files("src/main/kotlin"))
