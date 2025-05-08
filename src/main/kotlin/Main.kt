612e9bfae408ea58287306c76a81dd0ddf61b50d
Nadeen <75531472+NadeenXo@users.noreply.github.com>
2025-05-08 18:15:48 +0300

    fix imports problem

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 package org.baghdad
 
-import org.koin.core.context.GlobalContext.startKoin
 import di.appModule
 import kotlinx.coroutines.runBlocking
-import org.koin.core.context.startKoin
-import org.koin.core.context.stopKoin
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.core.context.GlobalContext.stopKoin
+
 
 fun main() {
     runBlocking {
